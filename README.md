96a70a3aad0c953f4be21e9309942af79d93c41a
ericpkatz <ericpkatz@gmail.com>
2022-09-01 18:07:37 -0400

    commit

---
+++ README.md
@@ -0,0 +1,12 @@
+- fork
+- npm i
+- npm run start:dev
+
+
+# requirements
+
+- add sequelize data models
+- add an api route to return data
+- use the data from the api route in the front end of your application
+- add a button to add data
+- add a button to remove data
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use('/dist', express.static('dist'));
+
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+const numbers = [1, 2, 3];
+
+app.get('/api/numbers', (req, res)=> {
+  res.send(numbers);
+});
+
+const port = process.env.PORT || 3000;
+
+const init = ()=> {
+app.listen(port, ()=> console.log(`listening on port ${port}`));
+}
+
+init();
