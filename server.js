41d9ea7d4591a6f286cadf5e3f09a9b89589814e
Stanley Lim <stannie@Stanleys-Air.lan>
2022-09-07 17:37:58 -0400

    seed database

--- server.js
+++ server.js
@@ -3,8 +3,28 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize('postgres://localhost/reactthing');
 
-const syncAndSeed = async () => {
+const HotSauce = db.define('hot_sauce', {
+  name: Sequelize.DataTypes.STRING,
+  intensity: Sequelize.DataTypes.DECIMAL,
+});
 
+const syncAndSeed = async () => {
+  try {
+    await db.sync({ force: true });
+
+    const hotsauces = [
+      { name: 'sriracha', intensity: 2 },
+      { name: 'wasabi', intensity: 8 },
+      { name: 'valentina', intensity: 10 },
+      { name: 'korean fire noodle', intensity: 69 },
+    ];
+
+    await Promise.all(
+      hotsauces.map(hotsauce => HotSauce.create(hotsauce))
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const app = express();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-
 const root = createRoot(document.querySelector('#root'));
-root.render(<hr />);
+root.render(<h1>hello</h1>);
