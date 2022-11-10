9d08273a75ac6433de3fa62863ce59029a2d390e
Stanley Lim <stannielim@fb.com>
2022-11-10 18:32:46 -0500

    add boilerplate code

--- server.js
+++ server.js
@@ -1,31 +1,5 @@
 const express = require('express');
-const Sequelize = require('sequelize');
-
-const db = new Sequelize('postgres://localhost/reactthing');
-
-const HotSauce = db.define('hot_sauce', {
-  name: Sequelize.DataTypes.STRING,
-  intensity: Sequelize.DataTypes.DECIMAL,
-});
-
-const syncAndSeed = async () => {
-  try {
-    await db.sync({ force: true });
-
-    const hotsauces = [
-      { name: 'sriracha', intensity: 2 },
-      { name: 'wasabi', intensity: 8 },
-      { name: 'valentina', intensity: 10 },
-      { name: 'korean fire noodle', intensity: 69 },
-    ];
-
-    await Promise.all(
-      hotsauces.map(hotsauce => HotSauce.create(hotsauce))
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
+const {faker} = require('@faker-js/faker');
 
 const app = express();
 const path = require('path');
@@ -35,28 +9,15 @@ app.use('/dist', express.static('dist'));
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
-app.get('/hotsauces', async (req, res, next) => {
-  res.send(await HotSauce.findAll());
+app.get('/products', async (req, res, next) => {
+  res.send(Array(100).fill().map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.product(),
+    price: faker.commerce.price(),
+  })));
 });
 
-app.post('/hotsauce', async (req, res, next) => {
-  console.log(req.body);
-  // we want to create a new database entry with the name and intensity that that user typed in
-  const { name, intensity } = req.body;
-  const hotsauce = await HotSauce.create({ name, intensity });
-  res.send(hotsauce);
-});
 
 const port = process.env.PORT || 3000;
 
-const init = async ()=> {
-  //sync database and seed data here
-  try {
-    await syncAndSeed();
-    app.listen(port, ()=> console.log(`listening on port ${port}`));
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-init();
+app.listen(port, ()=> console.log(`listening on port ${port}`));
--- src/components/App.js
+++ src/components/App.js
@@ -1,152 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-// this component will take in two props
-// 1. name
-// 2. intensity
-const Card = (props) => {
-  return (
-    <div style={{ border: '1px solid black', padding: '0.5rem', margin: '0.1rem', }}>
-      <h6>{props.name}</h6>
-      <p>This is the intensity: {props.intensity}</p>
-    </div>
-  );
-};
+import {Routes, Route} from 'react-router-dom';
 
-// we are passing 1 prop right now
-// the prop is "name" and the value is "kendal"
+import {Products} from './Products';
+
 const App = () => {
-  // whatever goes into useState(........) is the default value
-  // `num` is the `getter`. that's how you get the value
-  // `setNum` is the `setter`. that's how you set the value
-  // const [num, setNum] = useState(12); 
+  const [products, setProducts] = useState([]);
 
-  const [hotsauces, setHotsauces] = useState([]);
-
-  // we have to use lifecycle in order to obtain data
-  // what is react lifecycle?
-  // something that happens in each "stage" of the component's life
-  // what are the stages of the component's life?
-  // 1. when the component first gets onto the screen
-  // a. this triggers useEffect
-  // b. componentDidMount
-
-  // 2. when the component gets updated
-  // a. this also triggers useEffect
-  // b. componentDidUpdate
-
-  // 3. when the component gets off of the screen
-  // a. this also triggers useEffect
-  // b. componentDidUnmount
-
-  // just a regular old function
-  // takes in 2 arguments
-  // 1. a callback function
-  // a. this callback function gets executed when your component mounts
-
-  // dont worry about this one yet
-  // 2. an array
-  // a. this is an array of dependencies. whenever a variable in this array gets updated, the callback will be executed again
   useEffect(() => {
-    // what is a state?
-    // a state is some sort of data
-    // the cool thing about state is...
-    // whenever the data changes, it automatically re-renders the screen
-
-    const getData = async () => {
-      // this is a very very good place to get data from your backend
-      // PEMDAS
-
-      // if you await a promise, you get the value
-      const {data} = await axios.get('/hotsauces');
-      setHotsauces(data);
+    const getProducts = async() => {
+      const {data: products} = await axios.get('/products');
+      setProducts(products);
     };
 
-    getData();
+    getProducts();
   }, []);
 
-  // this gets executed twice
-  // 1. before the component gets mounted. the useEffect did not get called yet
-  // 2. after the component got mounted. the useEffect did get called
-  // console.log(hotsauces);
-
-  const [name, setName] = useState('');
-  const [intensity, setIntensity] = useState(0);
-
-  const onChangeName = (event) => {
-    // sets the name to whatever the user typed
-    setName(event.target.value);
-  }
-
-  const onChangeIntensity = (event) => {
-    setIntensity(event.target.value);
-  };
-
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    
-    // we want to pass in two pieces of data
-    // to this post request
-    // 1. name
-    // 2. intensity
-    const response = await axios.post('/hotsauce', { name, intensity });
-
-    // console.log(response.data);
-    // console.log(hotsauces); // [some items in here]
-
-    setHotsauces([...hotsauces, response.data]);
-  };
+  console.log(products);
 
   return (
-    <>
-      {/*
-      two things to know about an input field
-        1. you can set the value using javascript
-        2. you can have pass onChange function using javascript. whenever user types something, this function gets called
-      */}
-      <form onSubmit={onSubmit}>
-        <input placeholder="name" value={name} onChange={onChangeName} />
-        <input placeholder="intensity" value={intensity} onChange={onChangeIntensity} type='number' />
-        <button>Create</button>
-      </form>
-
-      {
-        // the key has to be 
-        // 1. fully unique
-        // 2. uniquely identifies that one item
-        hotsauces.map(hotsauce => <Card key={hotsauce.id} name={hotsauce.name} intensity={hotsauce.intensity} />)
-      }
-    </>
+    <Routes>
+      <Route path='/products' element={<Products />} />
+    </Routes>
   );
 };
 
 export default App;
-
-/*
-1. how to make a component
-2. passing props
-3. lifecycle (useEffect)
-4. state (useState)
-*/
-
-/*
-jsx
-2 modes- when i say "mode" i mean "jsx interprets your code in html / javascript"
-
-1. html mode: you get into html mode by doing <
-2. javascript mode: you get into js mode by doing {
-*/
-
-/*
-there are like 3 ways to write these 2 lines'
-1.
-const response = await axios.get('/hotsauces');
-const data = response.data;
-
-2.
-const data = (await axios.get('/hotsauces')).data;
-
-3. destructuring
-const {data: hotsauces} = await axios.get('/hotsauces');
-const {data: user} = await axios.get('/user');
-*/
